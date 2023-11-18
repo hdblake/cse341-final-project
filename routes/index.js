f7e7aa16d32ad5c3b5a79aaea8688db8c1dd0c11
André Pedrosa <ped17006@byui.edu>
2023-11-18 17:10:59 -0300

    Adding Swagger /api-docs route and documentation

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
-router.use("/recipes", require("./recipes"));
+// router.use("/recipes", require("./recipes"));
+router.use("/", require("./swagger"));
 
 module.exports = router;
