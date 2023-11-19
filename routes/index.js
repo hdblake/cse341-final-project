47ca216be2d3f2d30ce8a705ec176ec278acb8f1
Everton Alves <64697974+Everton543@users.noreply.github.com>
2023-11-19 00:05:22 -0300

    fix the swagger route back

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const recipes = require('../controller/recipes');
 
-router.get("/", require("./swagger"));
+router.use("/", require("./swagger"));
 router.get("/recipes", recipes.getAllRecipes);
 
 module.exports = router;
