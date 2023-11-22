95abe37f54a797de086970db1debe8dc5338456f
Everton Alves <64697974+Everton543@users.noreply.github.com>
2023-11-21 21:32:50 -0300

    Created all get functions

---
+++ controller/comments.js
@@ -0,0 +1,24 @@
+const { ObjectId } = require('mongodb');
+const mongodb = require('../db/connect');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const getAllComments = async (req, res, next) => {
+    const result = await await mongodb.getDb().db(process.env.DATABASE_NAME).collection('comments').find().toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result);
+};
+
+const getCommentsById = async (req, res, next) => {
+    const id = req.params.id;
+    const objectId = new ObjectId(id);
+    const result = await mongodb.getDb().db(process.env.DATABASE_NAME).collection('comments').findOne({ _id: objectId });
+    if (result) {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(result);
+    } else {
+        res.status(404).send('Comment not found');
+    }
+};
+
+module.exports = {getAllComments, getCommentsById};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const router = require("express").Router();
 const recipes = require('../controller/recipes');
+const users = require('../controller/users');
+const comments = require('../controller/comments');
+const ratings = require('../controller/ratings');
 
 router.use("/", require("./swagger"));
+
+//recipes
 router.get("/recipes", recipes.getAllRecipes);
+router.get("/recipes/:id", recipes.getRecipeById);
+router.get("/recipes/:id/comments", recipes.getRecipeComments);
+router.get("/recipes/:id/ratings", recipes.getRecipeRatings);
+
+//users
+router.get("/users", users.getAllUsers);
+router.get("/users/:id", users.getUsersById);
+
+//comments
+router.get("/comments", comments.getAllComments);
+router.get("/comments/:id", comments.getCommentsById);
+
+//ratings
+router.get("/ratings", ratings.getAllRatings);
+router.get("/ratings/:id", ratings.getRatinsById);
 
 module.exports = router;
