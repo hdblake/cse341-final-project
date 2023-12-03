974bd671a4456348c015d4ec63a7665d111fc5cd
= <=>
2023-12-02 18:38:57 -0600

    added delete routes

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const controller = require('../controllers');
 router.get('/', controller.users.getAllUsers);
 router.get('/:id', controller.users.getUsersById);
 router.post('/', controller.users.createNewUser);
+router.delete('/:id', controller.users.deleteUser);
 
 module.exports = router;
